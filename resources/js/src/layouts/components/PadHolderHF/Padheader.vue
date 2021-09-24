85567e5303bc5e471ae691602e6de161c1cbe055
victorWorker <victorworker@yandex.com>
2021-09-25 00:00:53 +0300

    Initial source code
    Initial source code

---
+++ resources/js/src/layouts/components/PadHolderHF/Padheader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="relative">
+
+      <!-- Removable Item -->
+      <div>
+        buttons
+      </div>
+      <div>
+        <img src="@assets/images/padHolder/padholderavata.png" alt="">
+      </div>
+      <div>
+        alerts
+      </div>
+    </div>
+</template>
---
+++ resources/js/src/layouts/shop-layout/Shoplayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="layout--full-page">
+      <Padheader />
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import Padheader from "@/layouts/components/PadHolderHF/Padheader.vue"
+export default {
+  components: {
+    Padheader
+  }
+}
+</script>
